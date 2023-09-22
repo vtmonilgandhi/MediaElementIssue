4aa42a6c792ea8a6c14022e1ac8c533fc48dcb40
Monil Gandhi <monil.gandhi@volansys.com>
2023-09-22 14:55:05 +0530

    Added project

---
+++ MyApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Maui.Core.Primitives;
+using CommunityToolkit.Maui.Views;
+using System.ComponentModel;
+
+namespace MyApp;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+        Monkeys = new ObservableCollection<string>
+        {
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+        };
+
+        colview.ItemsSource = Monkeys;
+    }
+
+    public ObservableCollection<String> Monkeys { get; private set; }
+}
+
